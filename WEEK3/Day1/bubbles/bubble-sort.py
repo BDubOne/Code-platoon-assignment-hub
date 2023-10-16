838692c935112ab7d21a9a4e288aae41c25514cd
BDubOne <bryan.t.bartell@gmail.com>
2023-10-16 15:17:49 -0500

    pretty bubbles in the air

--- WEEK3/Day1/bubbles/bubble-sort.py
+++ WEEK3/Day1/bubbles/bubble-sort.py
@@ -1,7 +1,53 @@
 sequence = [4, 3, 5, 0, 1]
 swaps = 0
 
-# Your Code Here
+import random
 
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+arr = [random.randint(1, 10) for _ in range(10)]
+
+def bubble_swap(arr):
+    print(f'Initial array: {arr}')
+    swaps = 0
+
+    for i in range(len(arr)):
+        if arr[i] < arr[i - 1]:
+            arr[i], arr[i-1] = arr[i-1], arr[i]
+            swaps += 1
+    print(f'{swaps} swaps')
+    print(f'result: {arr}')
+bubble_swap(arr)
+        
+    
+
+def big_bubble_swap(arr):
+    print(f'Initial array: {arr}')
+    swaps = 0
+   
+    
+
+    for _ in range(len(arr)):
+        made_swap = False
+        for i in range(1, len(arr)):
+            if arr[i] < arr[i-1]:
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
+                swaps += 1
+                made_swap = True
+        if not made_swap:
+            break
+      
+   
+
+   
+    print(f"Sorted array: {arr}")
+    print(f"Swaps: {swaps}")
+
+# big_bubble_swap(arr)
+
+def sorting(arr):
+    print(f"Initial array: {arr}")
+    
+    sorted_arr = sorted(arr)
+    print(f"Sorted array using 'sorted()': {sorted_arr}")
+    print(f"original array remains unchanged: {arr}")
+
+# sorting(arr)
