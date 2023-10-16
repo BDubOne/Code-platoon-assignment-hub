0f77f533337877cbc1cd6d6f50494987a3e561c7
BDubOne <bryan.t.bartell@gmail.com>
2023-10-16 15:40:37 -0500

    playing with bubbles

--- WEEK3/Day1/bubbles/bubbleSort.js
+++ WEEK3/Day1/bubbles/bubbleSort.js
@@ -1,7 +1,51 @@
-var sequence = [4, 3, 5, 0, 1]
-var swaps = 0
+// var sequence = [4, 3, 5, 0, 1]
+// var swaps = 0
 
-// Your Code Here
+const arrayLength = 10;
+const maxRandomValue = 10
 
-console.log("Final result: " + result)
-console.log("Swaps: " + swaps)
+const randomArray = Array.from({length: arrayLength }, () => Math.floor(Math.random() * maxRandomValue) + 1);
+
+console.log(randomArray)
+
+const bubblesort = (arr) => {
+    let n = arr.length;
+    let swaps = 0;
+
+    for (let i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            [arr[i], arr[i +1]] = [arr[i + 1], arr[i]]
+            swaps += 1
+        } 
+    }
+    console.log("swaps = " + swaps)
+    return arr
+
+}
+const blowingBubbles = bubblesort(randomArray)
+console.log("blowing bubbles: " + blowingBubbles)
+
+// const bigBubbleSort = (arr) => {
+//     let n = arr.length;
+//     let swapped;
+//     let swaps = 0;
+//     do {
+//         swapped = false;
+//         for (let i = 0; i < n - 1; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+//                 swapped = true;
+//                 swaps += 1
+//             }
+//         } 
+//         n--;
+//     } while (swapped);
+//     console.log("Swaps: " + swaps)
+//     return arr;
+
+// }
+
+// const sortedArr = bigBubbleSort(randomArray);
+// console.log("Sorted Array:", sortedArr);
+
+
