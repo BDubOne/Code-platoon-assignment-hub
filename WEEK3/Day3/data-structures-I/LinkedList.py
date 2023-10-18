89cecd4962a61d3199dcd7808c7351d4d479ba0a
BDubOne <bryan.t.bartell@gmail.com>
2023-10-18 08:20:57 -0500

    today's assignments

---
+++ WEEK3/Day3/data-structures-I/LinkedList.py
@@ -0,0 +1,13 @@
+####API####
+# insert - add node to beginning (or end)
+# search - find and return a given node
+# remove - remove a given node
+# size - return number of nodes
+# is_empty - True if no nodes, False otherwise
+
+class LinkedList:
+    def __init__(self):
+        pass
+
+    def insert(self,node):
+        self.append(node)
