20ffd79d58519fc5f4ea51032b9cdb08aeb74edc
BDubOne <bryan.t.bartell@gmail.com>
2023-10-24 07:51:23 -0500

    beginning to design portfolio page

--- WEEK4/Day1/todo/index.js
+++ WEEK4/Day1/todo/index.js
@@ -2,23 +2,34 @@ const taskInput = document.getElementById('task-input');
 const addButton = document.getElementById("add-button");
 const taskList = document.querySelector('ul');
 
-addTask = () => {
+ function addTask() {
     const taskIn = taskInput.value.trim();
     
     if (taskIn !== '') {
         const listItem = document.createElement('li');
-        listItem.innerHTML = `<span>${taskIn}</span> <button class="mark-complete">Mark Comlete</button>`;
+        listItem.innerHTML = `<span>${taskIn}</span><input type="checkbox" class="mark-complete"/> <button class="remove-task">Squash Beef</button>`;
 
         taskList.appendChild(listItem);
 
-        taskInput.value
+        taskInput.value = '';
     }
 }
+
 
 addButton.addEventListener('click', addTask);
 
 taskInput.addEventListener('keypress', function(event) {
     if (event.key === "Enter") {
         addTask();
+    }
+});
+
+taskList.addEventListener('click', function(event) {
+    if (event.target.classList.contains('mark-complete')) {
+        const taskItem = event.target.parentElement;
+        taskItem.querySelector('span').classList.toggle('completed');
+    } else if (event.target.classList.contains('remove-task')) {
+        const taskItem = event.target.parentElement;
+        taskItem.remove();
     }
 });
