799fb1bbf2e413b339fb5b4fbeb0fd54b38d4c5b
BDubOne <bryan.t.bartell@gmail.com>
2023-10-31 08:49:32 -0500

    today's assignments

--- WEEK5/video-store/app.js
+++ WEEK5/video-store/app.js
@@ -1,25 +1,86 @@
-const uuid = require('uuid')
+import inventoryJson from './video-store-inventory.json' assert {type:"json"};
+import { v4 as uuidv4 } from 'uuid';
+import { format, parse } from 'date-fns'
 
-const uuidv4 = uuid.v4;
-console.log('hello, world!')
 
-const myUUID = uuidv4();
-console.log('uuid is ', myUUID);
-
-function createStore() {
-    return {
-        customers: [],
-        videos: [],
-    }
+const state = {
+    customers: [],
+    videos: []
 }
 
-console.log('video store is ', createStore())
 
-function createCustomer(name) {
+const inventory = inventoryJson
+.map(video => {
+    const { id, ...rest } = video;
     return {
-        name: name.toLowerCase(),
-        id: uuidv4(),
-    }
+        imdbId: id,
+        ...rest
+    };
+})
+.map(video => {
+    return {
+
+        ...video,
+        imdbId: uuidv4()
+    };
+})
+.map(video => {
+    return {
+        ...video,
+        _meta: {
+            createdAt: format(new Date(), "yyyy-MM-dd'T'HH:mm:ss")
+        }
+    };
+});
+
+state.videos.push(...inventory)
+
+console.log(state)
+
+const makeCustomer = (state, name, email, checkoutOut = []) => {
+    const customer = {
+        cxId: uuidv4(),
+        name: name,
+        email: email,
+        checkedOut: checkedOut
+    };
+    state.customers.push(customer)
+    return customer
+
 }
 
-console.log('new customer ', createCustomer ('Alice'));
+
+const getCustomerById = (state, cxId) => {
+    result = state.customers
+    .filter(customer => customer.cxId === cxId);
+    return result
+}
+
+const findCustomerByEmail = (state, email) => {
+    result = state.customers
+    .filter(customer =>customer.email === email)
+    return result[0]
+}
+
+const findCustomerByName = (state, name) => {
+    result = state.customers
+    .filter(customer => customer.name === name)
+    return result
+}
+
+const findVideoByImdbId = (state,imdbId) => {
+    result = state.videos
+    .filter(video => video.imdbId === imdbId)
+    return result[0]
+}
+
+const findVideoByTitle = (state, title) => {
+    result = state.videos
+    .filter(video => video.title === title)
+    return result
+}
+
+console.log(findVideoByTitle("Creed III"))
+
+
+
